b820da4fe7003b5e4f1db8f8c69ad2b9705a0a42
moranguinho005 <parkklaura@gmail.com>
2026-03-25 12:54:28 -0300

    feat (E01): adiciona limite de ingressos ao SetorEnum

--- src/venda_ingresso/enums/SetorEnum.java
+++ src/venda_ingresso/enums/SetorEnum.java
@@ -1,21 +1,24 @@
 package venda_ingresso.enums;
 
 public enum SetorEnum {
-    AMARELO("Amarelo", 180.00),
-    AZUL("Azul", 100.00),
-    BRANCO("Branco", 60.00),
-    VERDE("Verde", 350.00);
+    AMARELO("Amarelo", 180.00, 10),
+    AZUL("Azul", 100.00, 20),
+    BRANCO("Branco", 60.00, 30),
+    VERDE("Verde", 350.00, 5);
 
     private final String nome;
     private final double valor;
+    private final int limiteIngressos;
 
-    SetorEnum(String nome, double valor) {
+    SetorEnum(String nome, double valor, int limiteIngressos) {
         this.nome = nome;
         this.valor = valor;
+        this.limiteIngressos = limiteIngressos;
     }
 
     public String getNome() { return nome; }
     public double getValor() { return valor; }
+    public int getLimiteIngressos() { return limiteIngressos; }
 
     @Override
     public String toString() { return nome; }
